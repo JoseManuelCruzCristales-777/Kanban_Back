0d3479505c6b31906f90f4f14ed482b9a1354f5a
Cristales <majocc012@gmail.com>
2025-07-28 22:18:03 -0600

    Primer commit al nuevo repo

--- config/cors.php
+++ config/cors.php
@@ -7,6 +7,8 @@ return [
     'allowed_methods' => ['*'],
 
     'allowed_origins' => [
+        'http://localhost:4200',
+        'http://127.0.0.1:4200',
         'https://josemanuelcruzcristales-777.github.io',
         'https://josemanuelcruzcristales-777.github.io/login-app',
         'https://josemanuelcruzcristales-777.github.io/login-app/*',
@@ -20,6 +22,6 @@ return [
 
     'max_age' => 0,
 
-    'supports_credentials' => false,
+    'supports_credentials' => true,
 
 ];
